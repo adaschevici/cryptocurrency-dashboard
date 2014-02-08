0e3eac9a03be254c938110ec849818d3660c1945
scarIz <pinnygalore@gmail.com>
2014-02-08 00:24:41 +0000

    Refactored tracker data organization.

---
+++ api/my_trackers.php
@@ -0,0 +1,28 @@
+<?php
+return array(
+  'captain' => array(
+    'anc'  => array(
+      'coinID' => 'anc',
+      'user'   => "http://anc.coinpool.in/index.php?page=api&action=getuserstatus&api_key=a30da728af5733e9e520e86c57b3b202d4d05b2739e6696ce408c16054a8ef0a&id=3773",
+      'pool'   => "http://anc.coinpool.in/index.php?page=api&action=getpoolstatus&api_key=a30da728af5733e9e520e86c57b3b202d4d05b2739e6696ce408c16054a8ef0a"
+    ),
+    'doge' => array(
+      'coinID' => 'doge',
+      'user'   => "https://doge.hashfaster.com/index.php?page=api&action=getuserstatus&api_key=062a3c5b93cc493066dd14a96601f1c55a9731e89d2d853c5b22b4a5362fba8c&id=6390",
+      'pool'   => "https://doge.hashfaster.com/index.php?page=api&action=getpoolstatus&api_key=062a3c5b93cc493066dd14a96601f1c55a9731e89d2d853c5b22b4a5362fba8c"
+    ),
+      'csc' => array(
+      'coinID' => 'csc',
+      'user'   => "https://csc.hash.so/index.php?page=api&action=getuserstatus&api_key=147ac00c74892540db719ac659adbfa0cfe4469c69aa65addc45ff9a780bb52e&id=2106",
+      'pool'   => "https://csc.hash.so/index.php?page=api&action=getpoolstatus&api_key=147ac00c74892540db719ac659adbfa0cfe4469c69aa65addc45ff9a780bb52e"
+      ),
+    'wdc'  => array(
+      'coinID' => 'wdc',
+      'user'   => "https://wdc.nut2pools.com/index.php?page=api&action=getuserstatus&api_key=f34259378201e88d580d88f9df7f556baaaab4aa938d4c3c115a42636fd5d5cb&id=2290",
+      'pool'   => "https://wdc.nut2pools.com/index.php?page=api&action=getpoolstatus&api_key=f34259378201e88d580d88f9df7f556baaaab4aa938d4c3c115a42636fd5d5cb"
+    )
+  ),
+  'ben' => array()
+);
+
+?>
--- api/trackers.php
+++ api/trackers.php
@@ -1,43 +1,23 @@
 <?php
 /** Adding User Trackers:
 
-    @index   A unique identifier for the tracker.
+    Create a 'my_trackers.php' file and return an array as per the following:
+
+    @user   A unique identifier for the tracker.
     @name   (optional) The display name for the coin. If unspecified, the coin name itself will be used by default
     @coinID The identifier for the coin as referenced at www.cryptocoincharts.info
     @user   The url of your personal tracker
     @pool   The pool url
 
     e.g.
-    'jewmaster' => array(
-       'coinID' => 'doge',
-       'name'   => 'Akay is a massive jew'
-       'user'   => 'http://...'
-       'pool'   => 'http://...'
-    )
-    And yes mate, this file ain't shared!!
-*/
-
-return array(
-    'captain' => array(
-
-      'anc'  => array(
-        'coinID' => 'anc',
-        'user'   => "http://anc.coinpool.in/index.php?page=api&action=getuserstatus&api_key=a30da728af5733e9e520e86c57b3b202d4d05b2739e6696ce408c16054a8ef0a&id=3773",
-        'pool'   => "http://anc.coinpool.in/index.php?page=api&action=getpoolstatus&api_key=a30da728af5733e9e520e86c57b3b202d4d05b2739e6696ce408c16054a8ef0a"
-      ),
-      'doge' => array(
-        'coinID' => 'doge',
-        'user'   => "https://pool.dogechain.info/index.php?page=api&action=getuserstatus&api_key=a94f88ad26da4afd79134c29302b72e7bf24c72a5d36331fbda5271969295e93&id=34470",
-        'pool'   => "https://pool.dogechain.info/index.php?page=api&action=getpoolstatus&api_key=a94f88ad26da4afd79134c29302b72e7bf24c72a5d36331fbda5271969295e93"
-      ),
-      'wdc'  => array(
-        'coinID' => 'wdc',
-        'user'   => "https://wdc.nut2pools.com/index.php?page=api&action=getuserstatus&api_key=f34259378201e88d580d88f9df7f556baaaab4aa938d4c3c115a42636fd5d5cb&id=2290",
-        'pool'   => "https://wdc.nut2pools.com/index.php?page=api&action=getpoolstatus&api_key=f34259378201e88d580d88f9df7f556baaaab4aa938d4c3c115a42636fd5d5cb"
+    return array(
+      'user' => array(
+         'coinID' => 'doge',
+         'name'   => 'DogeCoin, bitches!'
+         'user'   => 'http://...'
+         'pool'   => 'http://...'
       )
-    ),
-
-    'ben' => array()
-);
-
+    );
+*/
+return require 'my_trackers.php';
 ?>
